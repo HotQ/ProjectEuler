de8a0ec07d8651029444e63d5a9794728c2eb875
HotQ <calmoaf@gmail.com>
2017-01-01 21:23:36 +0800

    it's a brand new day

--- arch/arch.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#include "arch.h"
-#include "../desc/desc.h"
-int IDxxx(int id){
-	PrintDescription(id);
-	printf("\nresult:  %d\n",listSolved[id-1]());
-}
